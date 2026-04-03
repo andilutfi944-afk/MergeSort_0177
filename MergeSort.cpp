cd31b2a1ba46b57c1a25e2a7b0e85dbb755d7e69
andilutfi944-afk <andilutfi944@gmail.com>
2026-04-03 15:32:20 +0700

    Menambahkan if untuk kondisi berhnti

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -34,7 +34,12 @@ void input()
     }
 }
 
-
 // create function mergesort
-void mergeSort(int low, int high){
+void mergeSort(int low, int high)
+{
+    if (low >= high) // step 1
+    {
+        return; // step 1.a
+    }
+
 }
